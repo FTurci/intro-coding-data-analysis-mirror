0f2624daa0d8be1b06a55c87288ace03f83f97a5
FTurci <turci@me.com>
2025-09-22 09:46:50 +0100

    empty

---
+++ quarto/replace_python_with_pyodide.sh
@@ -0,0 +1,16 @@
+find . -type f -name "*.qmd" -exec perl -i -pe '
+  if (/{python}/) {
+    s/{python}/{pyodide}/g;
+  }
+' {} +
+
+find . -type f -name "*.qmd" -exec perl -i -0777 -pe '
+  s/(\{pyodide\})(?!\n#\| caption)/$1\n#| caption: "▶ Ctrl\/Cmd+Enter | ⇥ Ctrl\/Cmd+] | ⇤ Ctrl\/Cmd+\["/g;
+' {} +
+
+
+# turn this on to collapse to one lione if there is a mistake
+
+find . -type f -name "*.qmd" -exec perl -i -0777 -pe '
+  s/(#\| caption: "▶ Ctrl\/Cmd\+Enter \| ⇥ Ctrl\/Cmd\+\] \| ⇤ Ctrl\/Cmd\+\[")[\s\S]*?(?=\n(?!#\| caption)|\Z)/$1/mg;
+' {} +
